00100eced24e5e9f73d86c9df7d32245af064e32
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-01-15 03:17:37 +0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const cwd = path.resolve('.');
+  const env = loadEnv(mode, cwd, '');
 
   return {
     plugins: [react()],
     base: './', 
     resolve: {
       alias: {
-        '@': path.resolve('./'),
+        '@': cwd,
       },
     },
     define: {
