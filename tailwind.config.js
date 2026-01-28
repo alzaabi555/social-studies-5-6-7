df1dd9f614e552a90048953b9cb25f06705dc40d
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-01-28 10:42:08 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./*.{js,ts,jsx,tsx}",                // للملفات في الجذر
-    "./components/**/*.{js,ts,jsx,tsx}",  // لمجلد المكونات
-    "./data/**/*.{js,ts,jsx,tsx}",        // لمجلد البيانات
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./constants.tsx",
+    "./types.ts"
   ],
   theme: {
     extend: {
       fontFamily: {
         tajawal: ['Tajawal', 'sans-serif'],
       },
-      // أزلنا كل الألوان والأنيميشن المخصصة لنعود للوضع الافتراضي
     },
   },
   plugins: [],
